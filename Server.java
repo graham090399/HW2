3b09524af6f4f9ede25e188cd9be5e498376d148
graham090399 <56526210+graham090399@users.noreply.github.com>
2019-10-13 21:38:21 -0500

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,99 @@
+package serverSide;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+import java.net.ServerSocket;
+
+import java.net.Socket;
+
+import java.util.Date;
+
+/**
+ * A TCP server that runs on port 7070. When a client connects, it
+ * <p>
+ * sends the client the current date and time, then closes the
+ * <p>
+ * connection with that client.
+ */
+
+public class Server
+{
+
+    /**
+     * Runs the server.
+     */
+
+    public static void main(String[] args) throws IOException
+    {
+
+        /* Create the socket listner
+
+         * using ServerSocket Java class
+
+         * for Listening on a socket.
+
+         * Use port 7070*/
+
+        /* TO DO here */
+
+        Socket socket = new Socket();
+        ServerSocket listner = new ServerSocket(7070);
+
+        System.out.println("ServerSocket running on port 7070");
+
+        try
+        {
+
+            while (true)
+            {
+
+                /** To Do
+                 * Accept client connection here
+
+                 */
+
+                /* TO DO here*/
+               socket = listner.accept();
+
+                try
+                {
+
+                    System.out.println("Now listening on port 9090");
+
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                    out.println(new Date().toString());
+
+                } finally
+                {
+
+                    System.out.println("terminated running on port 9090");
+
+                    /* close the communication channel here
+
+                     */
+
+                    /* To Do Here */
+                    listner.close();
+                    socket.close();
+                }
+
+            }
+
+        } finally
+        {
+
+            /* close the communication channel here
+
+             */
+
+            /* To Do Here */
+            listner.close();
+            socket.close();
+        }
+
+    }
+
+}
